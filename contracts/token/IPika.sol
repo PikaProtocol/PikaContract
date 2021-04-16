83fe9c2c02496cb7ffbfa7bf4b3526bf01df281a
ethandev0 <ethandev@protonmail.com>
2021-04-16 12:05:44 -0700

    added reward distributor

--- contracts/token/IPika.sol
+++ contracts/token/IPika.sol
@@ -5,4 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 interface IPika is IERC20 {
     function mint(address to, uint256 amount) external;
     function burn(address from, uint256 amount) external;
+    function totalSupplyWithReward() external view returns (uint256);
+    function balanceWithReward(address account) external view returns (uint256);
 }
